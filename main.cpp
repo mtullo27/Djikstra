80ddd85c43dfd13235eb90ad5e52017759e1700b
Matthew Tullo <mtullo1@binghamton.edu>
2020-11-22 18:14:15 -0500

    commit

--- main.cpp
+++ main.cpp
@@ -14,27 +14,29 @@ using namespace std;
 int min(int a, int b){
     return (a > b)? a:b;
 }
-vector<vector<int>> getMatrix(vector<string> v){
-    vector<vector<int>> ret;
-    for(int i = 0; i<v.size(); i++){
-        for(int j = 0; j<v[i].size(); j++){
-            string s = v[i];
-            if(s[j].compare(" ")!=0){
-                ret[i].push_back(stoi(s[j]));
-            }
-        }
+
+//helper function to print vectors
+void printMatrix(vector<vector<int>> v){
+  for(int i = 0; i < v.size(); i++){
+    for(int j = 0; i <v[i].size(); j++){
+      cout << v[i][j] << " ";
     }
+    cout << endl;
+  }
 }
-
-
-int main(int argc, char* argv[]){
-    //opening the input file
-    ifstream infile;
-    string file;
-    infile.open(argv[1]);
-    vector<string>lines;
-    while(getline(infile, file))
-        lines.push_back(file);
-    vector<vector<int>> matrix = getMatrix(lines);
-    return 0;
+int main(){
+    //setting up the inputs
+  vector<vector<int>> in1;
+  vector<vector<int>> in2;
+  vector<vector<int>> in3;
+  //Test a in vector in1, b in in2 and c in in3
+  in2[0] = {0, 2, 4, -1, -1, 7, -1};
+  in2[1] = {2, 0, 3, 3, -1, -1, -1};
+  in2[2] = {4, 3, 0, 4, 3, 8, -1};
+  in2[3] = {-1, 3, 4, 0, 6, -1, -1};
+  in2[4] = {-1, -1, 3, 6, 0, 6, 8};
+  in2[5] = {7, -1, 8, -1, 6, 0, 12};
+  in2[6] = {-1, -1, -1, -1, 8, 12, 0};
+  printMatrix(in2);
+  return 0;
 }
